280e6e5455d91e9ea6d279dd40625729cb42f6e6
aaneleh <helenakurzgon@gmail.com>
2022-02-12 14:21:07 -0300

    new models and overflow fixed

--- main.js
+++ main.js
@@ -12,27 +12,21 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 //CAMERA
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000)
-camera.position.set(8,4,10); //direita/esquerda, baixo/cima, frente/tras
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 100)
+camera.position.set(2,2,5);
 
 //LIGHT
 const ambientLight = new THREE.AmbientLight(0xffffff);
-ambientLight.position.set(5,5,5);
 scene.add(ambientLight);
-
 
 //MEU MODELO
 const loader = new GLTFLoader();
 
-loader.load( '/assets/models/Hamburger.glb', function ( gltf ) {
-	scene.add( gltf.scene );
+loader.load( '/assets/models/Home.glb', function (gltf) {
+	scene.add(gltf.scene);
 }, undefined, function ( error ) {
 	console.error( error );
 } );
-
-//HELPERS
-const gridHelper = new THREE.GridHelper(100,10);
-scene.add(gridHelper);
 
 //ORBIT CONTROL
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -42,8 +36,30 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //ANIMATION LOOP
+
+function moveX(move){
+    //move foward
+    if(move > 0){
+        positionX += 0.1;
+    //move backwards
+    } else {
+        positionX -= 0.1;
+    }
+}
+
+function moveZ(move){
+    //move foward
+    if(move > 0){
+        positionZ += 0.1;
+    //move backwards
+    } else {
+        positionZ -= 0.1;
+    }
+}
+
 function animate(){
     requestAnimationFrame(animate);
+
     controls.update();
     renderer.render(scene, camera);
 }
