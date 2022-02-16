d675fc284f08c7aa9cc06a0c6292dbb929cc8ea2
aaneleh <helenakurzgon@gmail.com>
2022-02-16 18:53:03 -0300

    euvougritar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import './css/style.css';
-import * as THREE from 'three';
-import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+/* import * as THREE from './node_modules/three';
+import { OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'; */
 
 //SIDEBAR
 const sidebar_buttons = document.getElementsByClassName('toggleSidebar');
