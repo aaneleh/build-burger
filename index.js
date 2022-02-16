8633b160d112eac4f2e4e69920577f9c9ce44fb8
aaneleh <helenakurzgon@gmail.com>
2022-02-16 18:33:19 -0300

    AAAAH

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 import './style.css';
+import * as THREE from 'three';
+import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-console.log('openned main page js');
+
+//SIDEBAR
+const sidebar_buttons = document.getElementsByClassName('toggleSidebar');
+for(var i = 0; i < sidebar_buttons.length; i++){
+    sidebar_buttons[i].addEventListener('click', function() {
+        const sidebar = document.getElementById('sidebar');
+        sidebar.classList.toggle('active');
+    });
+}
+
+//STARTING SETUP
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('canvas'),
+})
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 100)
+camera.position.set(0,2,5);
+
+//LIGHT
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+scene.background = new THREE.Color(0xFFFFFF);
+
+//MEU MODELO
+var model;
+const loader = new GLTFLoader();
+loader.load( '/assets/models/Home.glb', function (gltf) {
+	model = gltf.scene;
+    scene.add(gltf.scene);
+});
+
+//ORBIT CONTROL
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+
+//RENDERER
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//ANIMATION LOOP
+function animate(){
+    requestAnimationFrame(animate);
+    model.rotation.y += 0.005;
+    controls.update();
+    renderer.render(scene, camera);
+}
+animate();
