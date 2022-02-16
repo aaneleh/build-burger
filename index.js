3d57011fe6f85492d052e2f0d3100764d79b7775
aaneleh <helenakurzgon@gmail.com>
2022-02-16 18:25:45 -0300

    aaaaaa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('openned main page js');
 
-import '/style.css';
+import './css/style.css';
 import * as THREE from 'three';
 import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
